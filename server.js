ba715ae2eb0b2f9c92c0db1eee6a1434a9fbf16d
Alexander Guyer <guyera@flip1.engr.oregonstate.edu>
2018-11-07 01:51:27 -0800

    Init node backend

---
+++ server.js
@@ -0,0 +1,13 @@
+/*
+ * Node.js backend for CS TAs, group 3, calendar web app, final project in CS 290
+ * Authors: Tristan Hilbert, Tyler Titsworth, Ryan Kennedy, Alexander Guyer
+ * Created on 11/7/2018 @ 1:46 am
+ *
+ */
+
+var express = require('express');
+var app = express();
+
+app.listen(80, function(){
+	console.log("Server listening on port 80");
+});
