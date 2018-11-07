99029a0af0f55e338d436cb416f3ab3c470d81a3
Alexander Guyer <guyera@flip1.engr.oregonstate.edu>
2018-11-07 02:29:54 -0800

    Added Mongo DB

--- server.js
+++ server.js
@@ -5,9 +5,45 @@
  *
  */
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'webdevfinal';
+const client = new MongoClient(url);
+var db;
+
+/*
+ *
+ * Connect to Mongo DB
+ *
+ */
+
+client.connect(function (err, client){
+	assert.equal(null, err);
+	console.log('Driver connected to Mongo DB');
+
+	db = client.db(dbName);
+});
+
+/*getEvents, takes in query parameters (http://host/events?start_date=<START_DATE>&end_date=<END_DATE>
+ * start_date and end_date mark the bounds of the database access. This way, if large calendars are stored, it is
+ * possible to request the events for only particular days. Range is inclusive. Returned as JSON object array for ease of
+ * integration.
+ *
+ */
+
+function getEvents (req, res){
+	res.json({});//For now, empty JSON object. TODO: implement function to gather objects from MongoDB.
+};
 
 app.listen(80, function(){
 	console.log("Server listening on port 80");
+	
+	//Serve static files in root web app directory
+	app.use(express.static('/'));
+
+	app.get('/events', getEvents);
 });
