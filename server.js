6a61f44f35f3a05abb11426cba2485faa74e2523
TheREK3R <kennerya@oregonstate.edu>
2018-11-20 18:14:08 -0800

    new file:   package-lock.json 	modified:   package.json 	modified:   server.js 	new file:   views/404.handlebars 	renamed:    public/index.html -> views/calendar.handlebars 	new file:   views/layouts/main.handlebars

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
  */
 
 const express = require('express');
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
@@ -14,36 +16,48 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'webdevfinal';
 const client = new MongoClient(url);
 var db;
+var port = process.env.PORT || 80;
 
-/*
- *
- * Connect to Mongo DB
- *
- */
 
-client.connect(function (err, client){
-	assert.equal(null, err);
-	console.log('Driver connected to Mongo DB');
+// /*
+//  *
+//  * Connect to Mongo DB
+//  *
+//  */
 
-	db = client.db(dbName);
+// client.connect(function (err, client){
+// 	assert.equal(null, err);
+// 	console.log('Driver connected to Mongo DB');
+
+// 	db = client.db(dbName);
+// });
+
+// /*getEvents, takes in query parameters (http://host/events?start_date=<START_DATE>&end_date=<END_DATE>
+//  * start_date and end_date mark the bounds of the database access. This way, if large calendars are stored, it is
+//  * possible to request the events for only particular days. Range is inclusive. Returned as JSON object array for ease of
+//  * integration.
+//  *
+//  */
+
+// function getEvents (req, res){
+// 	res.json({});//For now, empty JSON object. TODO: implement function to gather objects from MongoDB.
+// };
+
+//Set up express
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+app.use(express.static('public'));
+
+
+
+//serve webpage, will need updating
+app.get("/calendar", function(req, res, next){
+    res.status(200).render('calendar', {});
+});
+app.get("*", function(req, res, next){
+    res.status(404).render('404', {});
 });
 
-/*getEvents, takes in query parameters (http://host/events?start_date=<START_DATE>&end_date=<END_DATE>
- * start_date and end_date mark the bounds of the database access. This way, if large calendars are stored, it is
- * possible to request the events for only particular days. Range is inclusive. Returned as JSON object array for ease of
- * integration.
- *
- */
-
-function getEvents (req, res){
-	res.json({});//For now, empty JSON object. TODO: implement function to gather objects from MongoDB.
-};
-
-app.listen(80, function(){
-	console.log("Server listening on port 80");
-	
-	//Serve static files in root web app directory
-	app.use(express.static(__dirname + '/public'));
-
-	app.get('/events', getEvents);
+app.listen(port, function(){
+	console.log("Server listening on port", port);	
 });
