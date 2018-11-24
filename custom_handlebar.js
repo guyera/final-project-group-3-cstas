33fef5a2849f84ba8b3ef966def7adc822270753
TheREK3R <kennerya@oregonstate.edu>
2018-11-23 22:00:11 -0800

    modified:   custom_handlebar.js

--- custom_handlebar.js
+++ custom_handlebar.js
@@ -8,26 +8,9 @@
 //TODO
 //timeIndex time
 
-module.exports.getTimeIndex = function(args){
-    // console.log("==get Time Index is called!");
-    // console.log("==args: ", args);
-    var output = "";
-    for(var i = 0; i < args.length; i ++){
-        if (args[i][time].substring(args[i][time].length - 2, args[i][time].length) == "AM")
-            output += "<li><span class=\"time-text AM\">"
-        else
-            output += "<li><span class=\"time-text PM\">"
-        output += args[i][time].substring(0, args[i][time].length - 2) + "</span></li>"
-    }
-
-    return output;
-}
-
-
+//sorry i deleted your functions they were super cool but i made them templates to add the tags
 
 module.exports.attach_custom_handles = function(handlebar){
-    handlebar.registerHelper("timeIndex", module.exports.getTimeIndex);
-    handlebar.registerHelper("dayCol", module.exports.getDayColumn);
     handlebar.registerHelper("ifDay", function(day, check, out){
         if(check[day]){return String(out);}
         return "";
