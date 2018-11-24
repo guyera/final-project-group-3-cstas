e7077adbdd2ee080464a26e4b5b87f276ad67168
Alexander Guyer <guyera@oregonstate.edu>
2018-11-24 21:34:49 +0000

    Events are all now read from database for rendering

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ const app = express();
 const c = require('calendar');
 
 const MongoClient = require('mongodb').MongoClient;
+
 const assert = require('assert');
-const url = 'mongodb://localhost:27017';
-const dbName = 'webdevfinal';
-const client = new MongoClient(url);
+var mongoHost = process.env.MONGO_HOST || '35.235.123.219';
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER || 'csta';
+var mongoPassword = process.env.MONGO_PASSWORD || 'gcp-csta';
+var mongoDbName = process.env.MONGO_DB_NAME || 'final';
+var mongoUrl = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDbName;
 var db;
 var port = process.env.PORT || 80;
 
@@ -25,23 +29,15 @@ var port = process.env.PORT || 80;
 //  *
 //  */
 
-// client.connect(function (err, client){
-// 	assert.equal(null, err);
-// 	console.log('Driver connected to Mongo DB');
+MongoClient.connect(mongoUrl, function (err, client){
+	assert.equal(null, err);
+	console.log('Driver connected to Mongo DB');
 
-// 	db = client.db(dbName);
-// });
-
-// /*getEvents, takes in query parameters (http://host/events?start_date=<START_DATE>&end_date=<END_DATE>
-//  * start_date and end_date mark the bounds of the database access. This way, if large calendars are stored, it is
-//  * possible to request the events for only particular days. Range is inclusive. Returned as JSON object array for ease of
-//  * integration.
-//  *
-//  */
-
-// function getEvents (req, res){
-// 	res.json({});//For now, empty JSON object. TODO: implement function to gather objects from MongoDB.
-// };
+	db = client.db(mongoDbName);
+	app.listen(port, function(){
+		console.log("Server listening on port", port);	
+	});
+});
 
 //Set up Tristan's weird Templating
 const custom_handles = require("./custom_handlebar.js");
@@ -59,33 +55,36 @@ context["event"] = "*Event*";
 
 app.get("/event/:month/:week/:year/:time", function(req, res, next){
     //should search for event here
-    var event_pass = {};
-    event_pass['event'] = require("./event.json");
-    res.status(200).render('events', event_pass);
+    db.collection('event').find({}).toArray(function(err, eventDocs){
+    	var event_pass = {};
+	event_pass['event'] = eventDocs;
+        res.status(200).render('events', event_pass);
+    });
 });
 
 function renderCalendar(week, year, month, res, next){
-    var event = require("./event.json");
-    cal = new c.Calendar(1);
-    cal = cal.monthDays(year, month);
-    if(cal.length > week){
-	var contextClone = JSON.parse(JSON.stringify(context));
-        cal = cal[week];
-        for(var j = 0; j < event.length; j++){
-            if(event[j]["year"] == year && event[j]["month"] == month){
-                for(var i = 0; i < cal.length; i++){
-                    if(cal[i] == event[j]["day"]){
-                        contextClone["times"][event[j]["time"]][contextClone["day"][i]] = true;
-                    }
-                }
-            }
-        }
-        res.status(200).render('calendar_app', {'context': contextClone});
-    }
-    else{
-        console.log("bad");
-        next();
-    }
+    db.collection('event').find({}).toArray(function(err, event){
+        cal = new c.Calendar(1);
+   	cal = cal.monthDays(year, month);
+   	if(cal.length > week){
+		var contextClone = JSON.parse(JSON.stringify(context));
+       		cal = cal[week];
+      	 	for(var j = 0; j < event.length; j++){
+           		if(event[j]["year"] == year && event[j]["month"] == month){
+                		for(var i = 0; i < cal.length; i++){
+                   			if(cal[i] == event[j]["day"]){
+                      			 	contextClone["times"][event[j]["time"]][contextClone["day"][i]] = true;
+                    			}
+                		}
+            		}
+        	}
+        	res.status(200).render('calendar_app', {'context': contextClone});
+    	}
+    	else{
+        	console.log("bad");
+        	next();
+    	}
+    });
 }
 
 app.get("/:month/:week/:year", function(req, res, next){
@@ -101,13 +100,9 @@ app.get("/", function(req, res, next){
     var now = new Date();
     var date = new Date(now.getFullYear(), now.getMonth(), 1);//Move to first of month
     var week = Math.floor((now.getDate() + (date.getDay() == 0 ? 6 : (date.getDay() - 1)) - 1) / 7);
-    console.log('Week:', week, '\nYear:', now.getFullYear(), '\nMonth:', now.getMonth());
     renderCalendar(week, date.getFullYear(), date.getMonth(), res, next);
 });
 app.get("*", function(req, res, next){
     res.status(404).render('404', {});
 });
 
-app.listen(port, function(){
-	console.log("Server listening on port", port);	
-});
